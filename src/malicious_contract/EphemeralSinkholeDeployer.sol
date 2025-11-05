5136c6296f316f34cd3947a59d69c10e651fd8f1
Olacantona <olaomoju.olataiwo@gmail.com>
2025-11-05 04:58:25 +0100

    Updates to various scripts

--- src/malicious_contract/EphemeralSinkholeDeployer.sol
+++ src/malicious_contract/EphemeralSinkholeDeployer.sol
@@ -5,17 +5,20 @@ import "src/malicious_contract/Sinkhole.sol"; // Adjust path as needed
 contract EphemeralSinkholeDeployer {
     // This function creates the Sinkhole and immediately destroys it in the same transaction.
     // This is the ONLY way to guarantee the contract code is cleared post-Dencun.
-    function deployAndImplode(address payable destructionRecipient) external payable returns (address destroyedAddress) {
+    function deployAndImplode(address payable destructionRecipient)
+        external
+        payable
+        returns (address destroyedAddress)
+    {
         // 1. Create the Sinkhole contract
         // The Deployer becomes the Sinkhole's 'owner'.
         Sinkhole newSinkhole = new Sinkhole{value: msg.value}();
-        
-       // Return the address of the new contract
+
+        // Return the address of the new contract
         return address(newSinkhole);
 
         // 2. The Deployer calls implode (which uses suicide/selfdestruct)
         // Since the Deployer is the owner, this succeeds.
-        newSinkhole.implode(destructionRecipient); 
-        
+        newSinkhole.implode(destructionRecipient);
     }
 }
